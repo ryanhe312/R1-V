05d23951844e445d98cd360a9e0431f12a21c2ed
ryanhe312 <ryanhe312@gmail.com>
2025-02-17 22:48:39 +0800

    fix: accelerate training

--- src/open-r1-multimodal/run_grpo.sh
+++ src/open-r1-multimodal/run_grpo.sh
@@ -15,20 +15,22 @@ torchrun --nproc_per_node="4" \
     --dataset_name leonardPKU/clevr_cogen_a_train \
     --deepspeed local_scripts/zero2.json \
     --max_prompt_length 512 \
-    --max_completion_length 512 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --max_completion_length 256 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --learning_rate 2e-4 \
+    --optim "adamw_bnb_8bit" \
     --logging_steps 1 \
     --bf16 \
     --report_to wandb \
-    --gradient_checkpointing false \
+    --gradient_checkpointing true \
     --attn_implementation flash_attention_2 \
     --max_pixels 401408 \
     --num_train_epochs 2 \
     --run_name Qwen2-VL-2B-GRPO-CLEVR-70k \
     --save_steps 100 \
     --save_only_model true \
-    --num_generations 2 \
+    --num_generations 4 \
     --use_peft True \
     --lora_r 16 \
     --lora_alpha 16 \
